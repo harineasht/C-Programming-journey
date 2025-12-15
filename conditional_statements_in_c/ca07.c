d62dccebbd458f701db40a387463c805e318ea99
harineasht <harineash007@gmail.com>
2025-12-15 09:55:06 +0530

    Add files via upload

---
+++ conditional_statements_in_c/ca07.c
@@ -0,0 +1,16 @@
+// # 7th question
+#include <stdio.h>
+int main()
+{
+    int a;
+    scanf("%d", &a);
+    if (a % 10 == a / 10)
+    {
+        printf("1");
+    }
+    else
+    {
+        printf("0");
+    }
+    return 0;
+}
