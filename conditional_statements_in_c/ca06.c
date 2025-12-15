d678c863ccf0619f156b4c49fd517c0b87626713
harineasht <harineash007@gmail.com>
2025-12-15 09:53:34 +0530

    Add files via upload

---
+++ conditional_statements_in_c/ca06.c
@@ -0,0 +1,16 @@
+// # 6th question
+#include <stdio.h>
+int main()
+{
+    int a;
+    scanf("%d", &a);
+    if (a >= 50)
+    {
+        printf("1");
+    }
+    else
+    {
+        printf("0");
+    }
+    return 0;
+}
