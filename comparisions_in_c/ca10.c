85aabc4fcf21f84ac4fcea524d0ceef2ad567fb5
harineasht <harineash007@gmail.com>
2025-12-15 11:03:38 +0530

    Add files via upload

---
+++ comparisions_in_c/ca10.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int num, d0, d1;
+
+    scanf("%d", &num);
+
+    // Extract digits
+    d0 = num / 10; // digit 0 (tens)
+    d1 = num % 10; // digit 1 (ones)
+
+    // Check condition
+    if (d0 > d1)
+    {
+        printf("Success");
+    }
+    else
+    {
+        printf("Failure");
+    }
+
+    return 0;
+}
