84976b2a6559ed6aabad6f7180c95fca584e7684
harineasht <harineash007@gmail.com>
2025-12-15 11:00:35 +0530

    Add files via upload

---
+++ comparisions_in_c/ca02.c
@@ -0,0 +1,17 @@
+// Checking foor equality
+#include <stdio.h>
+int main()
+{
+    int num;
+    scanf("%d", &num);
+    if (num == 50)
+    {
+        printf("Failure");
+    }
+    else
+    {
+        printf("Success");
+    }
+
+    return 0;
+}
