86974eb525bfb884047c7c1c341521421917d29a
harineasht <harineash007@gmail.com>
2025-12-28 07:41:09 +0530

    Add files via upload

---
+++ while_loops_in_c/ca10.c
@@ -0,0 +1,19 @@
+// Program to print odd numbers from 11 to 100 having ones digit 5 using while loop
+#include <stdio.h>
+
+int main()
+{
+    int i = 11, a, sum = 0;
+
+    while (i <= 100)
+    {
+        a = (i / 10);
+        if (i % 2 == 1 && a == 7)
+        {
+            sum += i;
+        }
+        i++;
+    }
+    printf("%d", sum);
+    return 0;
+}
