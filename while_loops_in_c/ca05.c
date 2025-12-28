6899d892640f63803e53bc4927b5e79a38e2c2fe
harineasht <harineash007@gmail.com>
2025-12-28 07:35:26 +0530

    Add files via upload

---
+++ while_loops_in_c/ca05.c
@@ -0,0 +1,17 @@
+// Program to print odd numbers from 1 to 10 using while loop
+#include <stdio.h>
+
+int main()
+{
+    int i = 1;
+
+    while (i <= 10)
+    {
+        if (i % 2 == 1)
+        {
+            printf("\n%d", i);
+        }
+        i++;
+    }
+    return 0;
+}
