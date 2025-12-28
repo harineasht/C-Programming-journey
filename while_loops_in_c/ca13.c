b071dae14fa630f0de933fd418e2ba4f6d497990
harineasht <harineash007@gmail.com>
2025-12-28 07:43:09 +0530

    Add files via upload

---
+++ while_loops_in_c/ca13.c
@@ -0,0 +1,16 @@
+// Program to print reversed number
+#include <stdio.h>
+
+int main()
+{
+    int a, rev = 0;
+    printf("Enter a number:");
+    scanf("%d", &a);
+    while (a != 0)
+    {
+        rev = (rev * 10) + (a % 10);
+        a = a / 10;
+    }
+    printf("%d", rev);
+    return 0;
+}
