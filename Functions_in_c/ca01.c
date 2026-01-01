ea73285f7f61b205605f08c34ea1e192b27f7dc7
harineasht <harineash007@gmail.com>
2026-01-01 22:25:10 +0530

    Add files via upload

---
+++ Functions_in_c/ca01.c
@@ -0,0 +1,20 @@
+// Program to print sum of numbers from 1 to 5 using for loop
+#include <stdio.h>
+
+void disp_sum()
+{
+    int i, sum = 0;
+
+    for (i = 1; i <= 5; i++)
+    {
+        sum = sum + i;
+    }
+
+    printf("%d", sum);
+}
+
+int main()
+{
+    disp_sum();
+    return 0;
+}
